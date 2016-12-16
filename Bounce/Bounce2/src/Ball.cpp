701a9fe81c12049bd4ad76e1dc92c2bfd5210a78
r21nomi <ryotakosu@gmail.com>
2016-12-16 09:38:13 +0900

    Add Bounce2

---
+++ Bounce/Bounce2/src/Ball.cpp
@@ -0,0 +1,45 @@
+//
+//  Ball.cpp
+//  Bounce1
+//
+//  Created by Ryota Takemoto on 2016/12/14.
+//
+//
+
+#include "Ball.hpp"
+
+Ball::Ball(ofVec2f _position, float _mass, float _radius, ofColor _color) {
+    currentPosition = _position;
+    mass = _mass;
+    radius = _radius;
+    acceleration = ofVec2f(0, 0);
+    velocity = ofVec2f(0, 0);
+    color = _color;
+    currentStep = 0;
+}
+
+void Ball::applyForce(ofVec2f force) {
+    ofVec2f f = force / mass;
+    acceleration += f;
+}
+
+void Ball::update() {
+    velocity += acceleration;
+    currentPosition += velocity;
+    acceleration *= 0;
+}
+
+void Ball::draw() {
+    ofSetColor(color);
+    ofFill();
+    
+    ofDrawEllipse(currentPosition.x, currentPosition.y, radius * 2, radius * 2);
+}
+
+void Ball::checkEdges(float stairY, int step) {
+    if (step > currentStep && currentPosition.y > stairY - radius) {
+        currentPosition.y = stairY - radius;
+        velocity.y *= -1;
+        currentStep++;
+    }
+}
---
+++ Bounce/Bounce2/src/Stair.hpp
@@ -0,0 +1,29 @@
+//
+//  Stair.hpp
+//  Bounce2
+//
+//  Created by Ryota Takemoto on 2016/12/16.
+//
+//
+
+#ifndef Stair_hpp
+#define Stair_hpp
+
+#include "ofMain.h"
+
+class Stair {
+public:
+    Stair(int stepNum, ofVec2f position, float width, float height, ofColor color);
+    void draw();
+    int getStepNum();
+    float getStairY();
+    
+private:
+    int stepNum;
+    ofVec2f position;
+    float width;
+    float height;
+    ofColor color;
+};
+
+#endif /* Stair_hpp */
