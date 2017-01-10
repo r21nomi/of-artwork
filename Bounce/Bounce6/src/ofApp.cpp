35d2ffd18e4881559875ac960c2a14df629686ad
r21nomi <ryotakosu@gmail.com>
2017-01-10 09:18:55 +0900

    Add Bounce6

---
+++ Bounce/Bounce6/src/ofApp.cpp
@@ -0,0 +1,206 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofBackground(255);
+    
+    ballColor = ofColor(ofRandom(255), ofRandom(255), ofRandom(255));
+    stairColor = ofColor(ofRandom(255), ofRandom(255), ofRandom(255));
+    
+    initStairs();
+    
+    mainOutputSyphonServer.setName("Screen Output");
+    individualTextureSyphonServer.setName("Texture Output");
+    
+    client.setup();
+    client.set("","Simple Server");
+    
+    drawFbo.allocate(ofGetWidth(), ofGetHeight(), GL_RGBA);
+    maskFbo.allocate(ofGetWidth(), ofGetHeight(), GL_RGBA);
+    
+    skewLeft = 150;
+    skewRight = 150;
+}
+
+void ofApp::initStairs() {
+    int count = 6;
+    float alpha = 255 / count;
+    float height = ofGetHeight() / count;
+    stairs.clear();
+    for (int i = 0; i <= count; i++) {
+        float stairAlpha = ofMap(alpha * (i + 1), alpha, 255, 160, 255);
+        float y = i * height;
+        stairs.push_back(new Stair(i + 1, ofVec2f(0, y), ofGetWidth(), ofGetHeight() - y, ofColor(stairColor, stairAlpha)));
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    for (Ball* ball : balls) {
+        float g = 0.4;  // Fall faster if this value is bigger.
+        ofVec2f gravity = ofVec2f(0, g * ball->mass);
+        float c = 0.05;
+        ofVec2f friction = ball->velocity;
+        friction *= -1;
+        friction.normalize();
+        friction *= c;
+        
+        ball->applyForce(friction);
+        ball->applyForce(gravity);
+        ball->update();
+        
+        for (Stair* stair : stairs) {
+            ball->checkEdges(stair->getStairY(), stair->getStepNum());
+        }
+    }
+    vector<Ball*>::iterator it = balls.begin();
+    while (it != balls.end()) {
+        Ball *item = dynamic_cast<Ball*>(*it);
+        if (item->shouldDelete(ofGetHeight())) {
+            it = balls.erase(it);
+            break;
+        } else {
+            ++it;
+        }
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    ofSetColor(255);
+    
+    // Mask
+//    maskFbo.begin();
+//    
+//    ofClear(0, 0, 0, 0);
+//
+//    ofBeginShape();
+//    ofVertex(skewLeft, 0);
+//    ofVertex(ofGetWidth() - skewRight, 0);
+//    ofVertex(ofGetWidth(), ofGetHeight());
+//    ofVertex(0, ofGetHeight());
+//    ofVertex(skewLeft, 0);
+//    ofEndShape();
+//    
+//    maskFbo.end();
+    
+    // Draw
+    drawFbo.begin();
+    
+    for (Stair* stair : stairs) {
+        stair->draw();
+    }
+    
+    client.draw(50, 50);
+    
+    mainOutputSyphonServer.publishScreen();
+    drawFbo.end();
+    
+    drawFbo.getTexture().setAlphaMask(maskFbo.getTexture());
+    drawFbo.draw(0, 0);
+    
+    // Ball
+    for (Ball* ball : balls) {
+        ball->draw();
+    }
+}
+
+Ball* ofApp::getBall() {
+    float radius = 40;
+    float mass = 0.4;  // Bound faster if this value is smaller...why?
+    float x = ofRandom(skewLeft, ofGetWidth() - skewRight);
+    return new Ball(ofVec2f(x, ofRandom(0, ofGetHeight() / 5)), mass, radius, ofColor(ballColor, ofRandom(180, 255)));
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+    switch (key) {
+        case ' ':
+            balls.push_back(getBall());
+            break;
+            
+        case 'b':
+            ballColor = ofColor(ofRandom(255), ofRandom(255), ofRandom(255));
+            break;
+            
+        case 's':
+            stairColor = ofColor(ofRandom(255), ofRandom(255), ofRandom(255));
+            initStairs();
+            break;
+            
+        default:
+            break;
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+    Stair* stair = new Stair(stairs.size() + 1, ofVec2f(0, y), ofGetWidth(), ofGetHeight() - y, ofColor(stairColor, 255));
+    
+    // Insert to particular position.
+    vector<Stair*>::iterator it = stairs.begin();
+    while (it != stairs.end()) {
+        Stair *item = dynamic_cast<Stair*>(*it);
+        Stair *nextItem = dynamic_cast<Stair*>(*it + 1);
+        
+        if (nextItem != NULL && item->getStairY() > y && nextItem->getStairY() < y) {
+            it = stairs.insert(it, stair);
+            break;
+        } else {
+            ++it;
+        }
+    }
+    
+    int step = 1;
+    float alpha = 255 / stairs.size();
+    for (Stair* s : stairs) {
+        float stairAlpha = ofMap(alpha * step, alpha, 255, 160, 255);
+        s->update(step, ofColor(stairColor, stairAlpha));
+        step++;
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
